4e6ff636152f75e125b2ee63eb37c627850446aa
ChrisRSmithers <c.smithers@hotmail.co.uk>
2017-09-11 11:48:57 +0100

    Newer (correct) version

---
+++ InvalidDataException.java
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @author (Chris) 
+ * @version (a version number or a date)
+ */
+public class InvalidDataException extends Exception
+{
+    public InvalidDataException(){
+    super();
+    }
+    
+    public InvalidDataException(String message){
+    super(message);
+    }
+}
